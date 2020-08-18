77f5d8c7a5465463c706ce9f6cd6eb68bb2d2c43
junseongday <junseongday@gmail.com>
2020-08-18 22:52:59 +0900

    4장 예제 시작

--- Jpa4PrimaryKeyStratage/src/main/java/jpaBasic/start/Board.java
+++ Jpa4PrimaryKeyStratage/src/main/java/jpaBasic/start/Board.java
@@ -2,15 +2,50 @@ package jpaBasic.start;
 
 import javax.persistence.*;
 
+//@Entity
+//@SequenceGenerator(
+//        name = "BOARD_SEQ_GENERATOR",
+//        sequenceName = "BOARD_SEQ",
+//        initialValue = 1, allocationSize = 1
+//)
+//public class Board {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+//    private Long id;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//}
+
+//@Entity
+//@TableGenerator(
+//        name = "BOARD_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "BOARD_SEQ", allocationSize = 1
+//)
+//public class Board {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
+//    private Long id;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//}
+
 @Entity
-@SequenceGenerator(
-        name = "BOARD_SEQ_GENERATOR",
-        sequenceName = "BOARD_SEQ",
-        initialValue = 1, allocationSize = 1
-)
 public class Board {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Long getId() {
@@ -21,3 +56,4 @@ public class Board {
         this.id = id;
     }
 }
+
