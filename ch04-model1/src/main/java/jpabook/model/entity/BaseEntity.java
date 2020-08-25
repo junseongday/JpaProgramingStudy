83625d02f6a8c212deada9b7e24ba9fa1a4c7a95
junseongday <junseongday@gmail.com>
2020-08-25 22:29:49 +0900

    7장

---
+++ ch04-model1/src/main/java/jpabook/model/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package jpabook.model.entity;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+public class BaseEntity {
+    private Date createDate;
+    private Date lastModifiedDate;
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
--- ch04-model1/src/main/java/jpabook/model/entity/OrderItem.java
+++ ch04-model1/src/main/java/jpabook/model/entity/OrderItem.java
@@ -1,5 +1,7 @@
 package jpabook.model.entity;
 
+import jpabook.model.entity.item.Item;
+
 import javax.persistence.*;
 
 @Entity
